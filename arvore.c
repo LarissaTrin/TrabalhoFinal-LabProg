9e1db40e2f034d6f6c5d9cbe356c9a8bf81fec21
Lua <lala.trindade.2008@gmail.com>
2022-09-20 20:58:15 -0300

    arvore test

---
+++ arvore.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// cria a struct
+typedef struct nodeTree
+{
+    int fotoTabuleiroAtual;
+    int numTabuleiro;
+    struct nodeTree *filhos[8];
+    struct nodeTree *pai;
+    int quantChaves;
+} NodeTree;
+
+// cria um novo no e inicializa seus campos zerados
+NodeTree *inserirRaizTree(NodeTree *arvore, int tab, int numTab, NodeTree *ptPai)
+{
+    // printf("\nInserir raiz\n");
+    NodeTree *novoNode = malloc(sizeof(NodeTree));
+    if (arvore == NULL)
+    {
+        // printf("arvore NULL\n");
+        // é verdadeira penas a primeira vez para criar a raiz primaria
+        novoNode->fotoTabuleiroAtual = tab;
+        novoNode->numTabuleiro = numTab;
+        novoNode->pai = ptPai;
+        novoNode->filhos[0] = NULL;
+        novoNode->filhos[1] = NULL;
+        novoNode->filhos[2] = NULL;
+        novoNode->filhos[3] = NULL;
+        novoNode->filhos[4] = NULL;
+        novoNode->filhos[5] = NULL;
+        novoNode->filhos[6] = NULL;
+        novoNode->filhos[7] = NULL;
+    }
+    // printf("foto tab atual: %d\n", novoNode->fotoTabuleiroAtual);
+    return novoNode;
+}
+
+NodeTree *inserirFilhoTree(NodeTree *arvore, int numTab, int sugest[8])
+{
+    int i = 0;
+    while (i < 8 && sugest[i] != 0)
+    {
+        if (i == 0)
+        {
+            arvore->filhos[i] = inserirRaizTree(arvore->filhos[i], sugest[i], numTab, arvore);
+        }
+        if (i == 1)
+        {
+            arvore->filhos[i] = inserirRaizTree(arvore->filhos[i], sugest[i], numTab, arvore);
+        }
+        if (i == 2)
+        {
+            arvore->filhos[i] = inserirRaizTree(arvore->filhos[i], sugest[i], numTab, arvore);
+        }
+        if (i == 3)
+        {
+            arvore->filhos[i] = inserirRaizTree(arvore->filhos[i], sugest[i], numTab, arvore);
+        }
+        if (i == 4)
+        {
+            arvore->filhos[i] = inserirRaizTree(arvore->filhos[i], sugest[i], numTab, arvore);
+        }
+        if (i == 5)
+        {
+            arvore->filhos[i] = inserirRaizTree(arvore->filhos[i], sugest[i], numTab, arvore);
+        }
+        if (i == 6)
+        {
+            arvore->filhos[i] = inserirRaizTree(arvore->filhos[i], sugest[i], numTab, arvore);
+        }
+        if (i == 7)
+        {
+            arvore->filhos[i] = inserirRaizTree(arvore->filhos[i], sugest[i], numTab, arvore);
+        }
+        ++i;
+    }
+    return arvore;
+}
+
+int testFim(int sugestaoTest[8])
+{
+    int fim = 1;
+    for (int i = 0; i < 8; i++)
+    {
+        sugestaoTest[i] = sugestaoTest[i] * 10;
+        if (sugestaoTest[i] > 10)
+        {
+            fim = 0;
+        }
+    }
+    return fim;
+}
+
+void imprimir(NodeTree *raiz) {
+    if (raiz)
+    {
+        printf("%d   ", raiz->fotoTabuleiroAtual);
+        for (int i = 0; i < 8; i++)
+        {
+            imprimir(raiz->filhos[i]);
+        }
+    }
+    // printf("\n");    
+}
+
+int main()
+{
+
+    // char sugestao[8] = {'1','2','3','4','5','6','7','8'};
+    int fim = 1;
+    int tabuleiro = 20;
+    int sugestao[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    NodeTree *arvore = NULL;
+
+    arvore = inserirRaizTree(arvore, tabuleiro, 1, NULL);
+    printf("TEST: %d\n", arvore->fotoTabuleiroAtual);
+    NodeTree *aux = arvore;
+    int i = 0;
+    while (i < 5)
+    {
+        aux = inserirFilhoTree(aux, 1, sugestao);
+        aux = aux->filhos[0];
+        fim = testFim(sugestao);
+        ++i;
+    }
+    // printf("OI TO AQUI: %d", arvore->fotoTabuleiroAtual);
+    // printf("OI TO AQUI: %d", arvore->pai);
+    imprimir(arvore);
+    return 0;
+}
